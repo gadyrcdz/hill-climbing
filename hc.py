d9e1ccfe261317ece7c36a4b260693d00b3ee590
Leister Alvarado <leister.alvarado@gmail.com>
2026-03-23 20:57:11 -0600

    Remove bug of stale values

--- hc.py
+++ hc.py
@@ -21,23 +21,25 @@ import math
 
 def hill_climbing(map):
     current_map = map
-    current_cost = utils.cost(map)
-    last_cost = math.inf
+    current_cost = utils.cost(current_map)
 
-    while current_cost != last_cost:
-        last_cost = current_cost
+    while True:
+        best_map = current_map
+        best_cost = current_cost
 
         for hospital in utils.find_objects(current_map, utils.OBJECT_HOSPITAL):
-            neighbors = utils.actions(current_map, hospital)
-
-            for temp_hospital_move in neighbors:
+            for temp_hospital_move in utils.actions(current_map, hospital):
                 temp_map = utils.result(current_map, hospital, temp_hospital_move)
                 temp_cost = utils.cost(temp_map)
 
-                if current_cost > temp_cost:
-                    current_cost = temp_cost
-                    current_map = temp_map
+                if temp_cost < best_cost:
+                    best_map = temp_map
+                    best_cost = temp_cost
 
-                    neighbors = utils.actions(current_map, hospital)
+        if best_cost < current_cost:
+            current_map = best_map
+            current_cost = best_cost
+        else:
+            break
 
     return current_map
