b34cbfd7350810b4660cac304721bc076b588ceb
Gadir Calderón Díaz <99917841+gadyrcdz@users.noreply.github.com>
2026-04-16 15:30:03 -0600

    FEAT: To do functions finished

--- hc.py
+++ hc.py
@@ -1,4 +1,56 @@
+# import math
+
 import utils
+import random
+import math
+
+
+
+
+def hill_climbing(map):
+    """
+    Optimize a board by repeatedly choosing the best local move.
+
+    This function should implement classic hill climbing: on each iteration,
+    evaluate every one-step move for every hospital, choose the neighbor with
+    the lowest cost, and move there only if it strictly improves the current
+    cost. Stop when no improving neighbor exists.
+
+    Pseudocode (Python-style):
+    """
+    current_map = map
+    current_cost = utils.cost(current_map)
+
+    while True:
+        best_map = current_map
+        best_cost = current_cost
+
+        for hospital in utils.find_objects(current_map, utils.OBJECT_HOSPITAL):
+            for candidate_move in utils.actions(current_map, hospital):
+                candidate_map = utils.result(current_map, hospital, candidate_move)
+                candidate_cost = utils.cost(candidate_map)
+
+                if candidate_cost < best_cost:
+                    best_map = candidate_map
+                    best_cost = candidate_cost
+
+        if best_cost < current_cost:
+            current_map = best_map
+            current_cost = best_cost
+        else:
+            break   
+
+    return current_map
+    """
+    Args:
+        map: Matrix (list of lists) representing the board.
+
+    Returns:
+        list[list]: A map configuration with locally minimized cost.
+    """
+
+    # raise NotImplementedError("hill_climbing is not implemented yet")
+
 
 
 def simulated_annealing(grid, T_min, T_initial, cooling_rate):
@@ -16,31 +68,33 @@ def simulated_annealing(grid, T_min, T_initial, cooling_rate):
         list[list]: A grid configuration produced by the annealing search.
     """
 
-    # current grid <- grid
-    # current cost <- cost of the grid
-    # temperature <- initial temperature
+    current_grid = grid
+    current_cost = utils.cost(current_grid)
+    temperature =  T_initial
 
-    # while temperature is greater than the minimum temperature
-    #     movable hospitals <- hospitals that can move
+    while (temperature > T_min):
+        movable_hospitals = utils.find_objects(current_grid, utils.OBJECT_HOSPITAL)
+        if (len(movable_hospitals) == 0):
+            break
+        selected_hospital = random.choice(movable_hospitals)
+        
+        possible_moves = utils.actions(current_grid, selected_hospital)
+        if(len(possible_moves) == 0):
+            break
 
-    #     if movable hospitals is empty
-    #         stop the process
+        selected_move = random.choice(possible_moves)
+        neighbor_solution =  utils.result(current_grid, selected_hospital, selected_move)
+        neighbor_cost = utils.cost(neighbor_solution)
+        cost_difference =  neighbor_cost - current_cost
 
-    #     selected hospital <- a random movable hospital
-    #     possible moves <- valid moves for that hospital
-    #     selected move <- a random move
-    #     neighbor solution <- new grid produced by that move
-    #     neighbor cost <- cost of the neighbor solution
-    #     cost difference <- neighbor cost - current cost
+        if (neighbor_cost < current_cost):
+            current_grid = neighbor_solution
+        else:
+            acceptance_probability = math.exp(-cost_difference / temperature)
+            value = random.random()
+            if  (value < acceptance_probability):
+                current_grid = neighbor_solution
 
-    #     if the neighbor is better
-    #         accept the neighbor as the current solution
-    #     otherwise
-    #         acceptance probability <- value based on cost difference and temperature
+        temperature *= cooling_rate
 
-    #         random value <- random number between 0 and 1
-    #         if random value is less than the acceptance probability
-    #             accept the neighbor as the current solution
-
-    #     temperature <- temperature reduced by the cooling rate
-    # return current grid
+    return current_grid
--- tests/test_hill_climbing.py
+++ tests/test_hill_climbing.py
@@ -5,6 +5,45 @@ import pytest
 import utils
 
 
+
+def test_hill_climbing_reduces_cost_on_simple_line_map():
+    board = [[utils.OBJECT_HOSPITAL, None, None, None, utils.OBJECT_HOUSE]]
+    before = copy.deepcopy(board)
+    initial_cost = utils.cost(board)
+
+    solved = hc.hill_climbing(board)
+
+    assert board == before
+    assert utils.cost(solved) < initial_cost
+    assert solved == [[None, None, None, utils.OBJECT_HOSPITAL, utils.OBJECT_HOUSE]]
+
+
+def test_hill_climbing_stops_at_local_optimum():
+    board = [
+        [None, utils.OBJECT_HOUSE, None],
+        [utils.OBJECT_HOUSE, utils.OBJECT_HOSPITAL, utils.OBJECT_HOUSE],
+        [None, utils.OBJECT_HOUSE, None],
+    ]
+
+    solved = hc.hill_climbing(copy.deepcopy(board))
+    solved_cost = utils.cost(solved)
+
+    for hospital in utils.find_objects(solved, utils.OBJECT_HOSPITAL):
+        for candidate_move in utils.actions(solved, hospital):
+            candidate_map = utils.result(solved, hospital, candidate_move)
+            assert utils.cost(candidate_map) >= solved_cost
+
+
+def test_hill_climbing_does_not_mutate_input_map_when_improvement_exists():
+    board = [[utils.OBJECT_HOSPITAL, None, utils.OBJECT_HOUSE]]
+    before = copy.deepcopy(board)
+
+    solved = hc.hill_climbing(board)
+
+    assert board == before
+    assert solved != before
+
+
 def test_hill_climbing_reduces_cost_on_simple_line_map():
     board = [[utils.OBJECT_HOSPITAL, None, None, None, utils.OBJECT_HOUSE]]
     before = copy.deepcopy(board)
@@ -97,6 +136,7 @@ def test_simulated_annealing_handles_worse_moves_probabilistically(
 
     solved = hc.simulated_annealing(board, T_min=0.5, T_initial=1.0, cooling_rate=0.4)
 
+    
     assert board == before
     assert solved == expected_board
 
